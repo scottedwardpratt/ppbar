7211e2852d390067d552ae637f0be47212ca391d
scottedwardpratt <prattsc@msu.edu>
2020-08-13 16:04:08 -0400

    added some files

---
+++ scottrun/read3dhydro.cc
@@ -0,0 +1,37 @@
+#include "msu_sampler/master.h"
+#include <cstring>
+using namespace std;
+using namespace msu_sampler;
+
+
+int main(){
+	CparameterMap parmap;
+	parmap.ReadParsFromFile("hydro_parameters.txt");
+	CmasterSampler::meanfield=new CmeanField_Simple(&parmap);
+	CmasterSampler ms(&parmap);
+	long long int deltacount=0,rhocount=0;
+	CpartList pl=CpartList(&parmap);
+	ms.partlist=&pl;
+	ms.randy->reset(time(NULL));
+	
+	ms.ReadHyper();
+	
+	long long int nparts=0;
+	int	nevents=parmap.getI("SAMPLER_NEVENTS_TOT",10);
+	for(int ievent=0;ievent<nevents;ievent++){
+		nparts+=ms.MakeEvent();
+		deltacount+=ms.partlist->CountResonances(2224)+ms.partlist->CountResonances(-2224)
+			+ms.partlist->CountResonances(2214)+ms.partlist->CountResonances(-2214)
+				+ms.partlist->CountResonances(2224)+ms.partlist->CountResonances(-2224)
+					+ms.partlist->CountResonances(1114)+ms.partlist->CountResonances(-1114);
+		rhocount+=ms.partlist->CountResonances(113)+ms.partlist->CountResonances(213)+ms.partlist->CountResonances(-213);
+		if((10*(ievent+1))%nevents==0)
+			printf("After %d events, nparts/event=%g\n",ms.NEVENTS,double(nparts)/double(ms.NEVENTS));
+		//ms.partlist->Reset();
+	}
+	printf("Nrho=%g\n",double(rhocount)/double(nevents));
+	printf("Ndelta=%g\n",double(deltacount)/double(nevents));
+
+	printf("YIPPEE!!!!! We made it all the way through!\n");
+	return 0;
+}
