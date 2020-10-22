a1fbdc3b8eea255a856da6502286d6fb347ead49
scottedwardpratt <prattsc@msu.edu>
2020-10-22 13:18:50 -0400

    lots of changes to CF.cc

--- scottrun/hbt/hbt_ppbar.cc
+++ scottrun/hbt/hbt_ppbar.cc
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(){
 	CHBT_BES hbt("parameters.txt");
-	printf("howdy\n");
 	hbt.randy->reset(-time(NULL));
 	hbt.ReadPR();
 	hbt.CalcCF_MC();
+	//hbt.CFArray[0][0][10]->Print();
+	hbt.AverageCF();
 	return 0;
 }
