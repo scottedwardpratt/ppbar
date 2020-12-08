2f4b7231a5321352ceefc86d7b508f179b48bbc6
Karina Martirosova <karina@pratt-linux.nscl.msu.edu>
2020-12-08 18:56:51 -0500

    help

---
+++ software/src/calc_CF_parallel.cc
@@ -0,0 +1,74 @@
+//#ifndef __HBT_BES_CALC_CF_CC__
+//#define __HBT_BES_CALC_CF_CC__
+#include "commondefs.h"
+#include "coral.h"
+#include "hbt_bes.h"
+#include "omp.h"
+void CHBT_BES::CalcCF(){
+	CHBT_PartMap::iterator ita,itb;
+	int irap,iphi,iuperp;
+	vector<double> x(4);
+	CHBT_Part *partaa,*partbb;
+	CF *cf;
+	long long int ntot=0,ntotprime=0;
+	for(irap=0;irap<NRAP;irap++){
+		for(iphi=0;iphi<NPHI;iphi++){
+			ntot+=partmap[irap][iphi].size();
+			printf("irap=%d, iphi=%d, size=%d, ntot=%lld\n",irap,iphi,int(partmap[irap][iphi].size()),ntot);
+			for(ita=partmap[irap][iphi].begin();ita!=partmap[irap][iphi].end();++ita){
+				partaa=ita->second;
+				ntotprime+=1;
+				if(partaa->ID==IDA || partaa->ID==IDB){
+					itb=ita; itb++;
+					partbb=itb->second;
+					while(itb!=partmap[irap][iphi].end() && fabs(partaa->uperp-partbb->uperp)<UPERPTEST){
+						partbb=itb->second;
+						if((partaa->ID==IDA && partbb->ID==IDB) || (partaa->ID==IDB && partbb->ID==IDA)){
+							cf=GetCF(partaa,partbb);
+							if(cf!=NULL){
+								if(partaa->ID==IDA){
+									cf->Increment(partaa,partbb);
+								}
+								else
+									cf->Increment(partbb,partaa);
+							}
+						}
+						itb++;
+					}
+				}
+			}
+		}
+	}
+	for(irap=0;irap<NRAP;irap++){
+		for(iphi=0;iphi<NPHI;iphi++){
+			for(iuperp=0;iuperp<NUPERP;iuperp++){
+				CFArray[irap][iphi][iuperp]->Normalize();
+			}
+		}
+	}
+}
+
+void CHBT_BES::CalcCF_Gauss(double Rout,double Rside,double Rlong){
+	int imc;
+	vector<double> x;
+	double root2=sqrt(2.0);
+	x.resize(4);
+	x[0]=0.0;
+	cfgauss->nincrement=0;
+#pragma omp parallel for
+	for(imc=0;imc<NMC;imc++){
+		x[1]=root2*Rout*randy->ran_gauss();
+		x[2]=root2*Rside*randy->ran_gauss();
+		x[3]=root2*Rlong*randy->ran_gauss();
+		cfgauss->Increment(x);
+		if((10*(imc+1))%NMC==0){
+			printf("finished %g percent\n",100.0*(imc+1)/double(NMC));
+		}
+	}
+
+	
+	cfgauss->Normalize();
+}
+
+
+//#endif
