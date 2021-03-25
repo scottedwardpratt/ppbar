0db19bf8589523b7315666042c388bf07df2111d
Karina Martirosova <karina@pratt-linux.nscl.msu.edu>
2021-03-24 21:22:05 -0400

    update

--- karinarun/hbt/psicheck.cc
+++ karinarun/hbt/psicheck.cc
@@ -1,6 +1,9 @@
 #include "coral.h"
 #include "commondefs.h"
 #include "hbt_bes.h"
+#include "wavefunction.h"
+#include "constants.h"
+#include "sf.h"
 
 int CF::NQ=40;
 double CF::DELQ=2.0;
@@ -24,27 +27,31 @@ int main(){
 	x[1]=root2*4*randy->ran_gauss();
 	x[2]=root2*5*randy->ran_gauss();
 	int iq;
-	double phi,ctheta,stheta,qx,qy,qz;
+	double phi,ctheta,stheta,qx,qy,qz,q;
 	double r,psisquared,psi,ctheta_qr;
 	string filename="parameters/parameters_pp.txt";
-	CWaveFunction *wf=new CWaveFunction_pp_schrod(filename);
+	CWaveFunction *wf=new CWaveFunction_generic(filename, -1, 938.272, 938.272, 0.5);
 	r=sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
-	phi=2.0*PI*randy->ran();
-	ctheta=-1.0+2.0*randy->ran();
-	stheta=sqrt(1.0-ctheta*ctheta);
-	qz=ctheta;
-	qx=stheta*cos(phi);
-	qy=stheta*sin(phi);
-	ctheta_qr=(qx*x[0]+qy*x[1]+qz*x[2])/r;
 	
-	for(iq=0; iq<1000; iq++){
-		psisquared=wf->CalcPsiSquared(iq,r,ctheta_qr);
-		psi=wf->CalcPsiSquared(-iq,-r,ctheta_qr);
+	
+//	for(iq=0; iq<NSAMPLE_THETAPHI; iq++){
+		phi=2.0*PI*randy->ran();
+		ctheta=-1.0+2.0*randy->ran();
+		stheta=sqrt(1.0-ctheta*ctheta);
+		qz=ctheta;
+		qx=stheta*cos(phi);
+		qy=stheta*sin(phi);
+		q=sqrt(qx*qx+qy*qy+qz*qz);
+		ctheta_qr=(qx*x[0]+qy*x[1]+qz*x[2])/r;
+		
+		psisquared=wf->GetPsiSquared(q,r,ctheta_qr);
+		psi=wf->GetPsiSquared(-q,r,ctheta_qr);
 		if(psisquared != psi){
 			printf("Psi error");
 			exit (1);
 		}
-	}
+//	}
 	
 	return 0;
 }
+
