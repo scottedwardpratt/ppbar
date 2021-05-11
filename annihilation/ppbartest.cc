bea4c989162ef27466cc26cb9c21e80754e5b56d
scottedwardpratt <prattsc@msu.edu>
2021-05-11 12:43:49 -0400

    LOTS of fixes

--- annihilation/ppbartest.cc
+++ annihilation/ppbartest.cc
@@ -10,6 +10,8 @@ int main(int argc,char *argv[]){
 		exit(1);
 	}
 	parsfilename=argv[1];
+	char filename[100];
+	FILE *fptr;
 	double q,r,ctheta,x,y,z,Rx,Ry,Rz,psi2,delq;
 	vector<double>psi2bar;
 	unsigned long long int imc,nmc;
@@ -17,32 +19,27 @@ int main(int argc,char *argv[]){
 	CWaveFunction *wf;
 	//wf=new CWaveFunction_ppbar_nocoulomb(parsfilename);
 	wf=new CWaveFunction_optical(parsfilename);
-	printf("wf created\n");
 	nmc=wf->parameters.getI("NMC",1000);
 	CRandy *randy=new CRandy(time(NULL));
 	delq=wf->GetDELQ();
 	nqmax=wf->GetNQMAX();
 	psi2bar.resize(nqmax);
-	for(iq=0;iq<nqmax;iq++){
-		psi2bar[iq]=0.0;
-	}
 	Rx=Ry=Rz=6.0;
-	for(imc=0;imc<nmc;imc++){
-		x=Rx*randy->ran_gauss();
-		y=Ry*randy->ran_gauss();
-		z=Rz*randy->ran_gauss();
-		r=sqrt(x*x+y*y+z*z);
-		ctheta=1.0-2.0*randy->ran();
-		for(iq=0;iq<nqmax;iq++){
-			psi2=wf->GetPsiSquared(iq,r,ctheta);
+	sprintf(filename,"CF_Rx%gRy%gRz%g.txt",Rx,Ry,Rz);
+	fptr=fopen(filename,"w");
+	for(iq=0;iq<nqmax;iq++){
+		q=(0.5+iq)*delq;
+		psi2bar[iq]=0.0;
+		for(imc=0;imc<nmc;imc++){
+			x=Rx*randy->ran_gauss();
+			y=Ry*randy->ran_gauss();
+			z=Rz*randy->ran_gauss();
+			r=sqrt(x*x+y*y+z*z);
+			ctheta=1.0-2.0*randy->ran();
+			psi2=wf->GetPsiSquared(q,r,ctheta);
+			//psi2=wf->CalcPsiSquared(iq,r,ctheta);
 			psi2bar[iq]+=psi2;
 		}
-	}
-	char filename[100];
-	sprintf(filename,"CF_Rx%gRy%gRz%g.txt",Rx,Ry,Rz);
-	FILE *fptr=fopen(filename,"w");
-	for(iq=0;iq<nqmax;iq++){
-		q=(iq+0.5)*delq;
 		printf("%5.1f %g\n",q,psi2bar[iq]/double(nmc));
 		fprintf(fptr,"%5.1f %g\n",q,psi2bar[iq]/double(nmc));
 	}
